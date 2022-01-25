6f732794fbb1df9797c6cc625fce968d212023a0
Daniel <daniel.campos@picpay.com>
2022-01-25 09:19:24 -0300

    feat: dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import AppRoutes from './routes'
 import { BrowserRouter } from 'react-router-dom'
+import GlobalStyle from './styles/global'
 
 const App: React.FC = () => (
-    <BrowserRouter>
-        <AppRoutes />
-    </BrowserRouter>
+    <>
+        <BrowserRouter>
+            <AppRoutes />
+        </BrowserRouter>
+        <GlobalStyle />
+    </>
 )
 
 export default App;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Title, Form, Repositories } from './styles'
+import { FiChevronRight } from 'react-icons/fi';
+import logo from '../../assets/github-logo-md.png'
 
 const Dashboard: React.FC = () => (
-    <h1>Explore repositórios no GitHub</h1>
+    <>
+    <img src={logo} alt="GitHub Explorer" />
+    <Title>Explore repositórios no GitHub</Title>
+    <Form>
+        <input type="text" placeholder="Digite o repositório" />
+        <button type="submit">Pesquisar</button>
+    </Form>
+    <Repositories>
+        <a href="">
+            <img src="https://avatars.githubusercontent.com/u/60238933?v=4" alt="foto do usuário" />
+            <div>
+                <strong>rocketseat/unform</strong>
+                <p>Easy peasy highly scalable ReactJS & React Native forms</p>
+            </div>
+            <FiChevronRight size={20}/>
+        </a>
+    </Repositories>
+    </>
 )
 
 export default Dashboard;
